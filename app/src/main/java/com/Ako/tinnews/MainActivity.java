ac006cdb2a9ad691cf6885fcd2d8b67e11f9ae59
AkoZhu <zxuanbiao@outlook.com>
2023-02-03 21:18:36 -0500

    Finish the JetPack Navigation in Bottom menu

--- app/src/main/java/com/Ako/tinnews/MainActivity.java
+++ app/src/main/java/com/Ako/tinnews/MainActivity.java
@@ -1,14 +1,43 @@
 package com.Ako.tinnews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.Ako.tinnews.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        BottomNavigationView bottomNavigationView = binding.navView;
+        //NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(binding.navHostFragment.getId());
+        NavHostFragment navHostFragment = binding.navHostFragment.getFragment();
+        navController = navHostFragment.getNavController();
+
+        // click on tab on BottomView can navigate to different fragment
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        // Can click back.
+        return navController.navigateUp();
+    }
+
 }
