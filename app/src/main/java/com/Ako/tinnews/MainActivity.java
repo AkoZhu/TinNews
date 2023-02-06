b8e4417beb424f73970ca31a9f0eb9c370e8d32f
AkoZhu <zxuanbiao@outlook.com>
2023-02-05 22:09:10 -0500

    some bug

--- app/src/main/java/com/Ako/tinnews/MainActivity.java
+++ app/src/main/java/com/Ako/tinnews/MainActivity.java
@@ -7,9 +7,17 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.Ako.tinnews.databinding.ActivityMainBinding;
+import com.Ako.tinnews.model.NewsResponse;
+import com.Ako.tinnews.network.NewsApi;
+import com.Ako.tinnews.network.RetrofitClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +40,35 @@ public class MainActivity extends AppCompatActivity {
         // click on tab on BottomView can navigate to different fragment
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+        // new task, make the call<NewsResponse> in NewsApi.java
+        // add task to queue
+        // while(true) {retrofit keep check the queue}
+        // If queue has task, retrofit do task: call endpoint, parse json, etc.
+        // Once retrofit finish the task
+        // callback.onResponse(response)
+        // if (task success) newsResponseCallback.onResponse()
+        // else newsResponseCallback.onFailure()
+
+        // Use Retrofit.create(yourClass.class) to create your interface object
+        NewsApi api = RetrofitClient.newInstance().create(NewsApi.class);
+        // Call<NewsResponse>
+        api.getTopHeadlines("us").enqueue(new Callback<NewsResponse>() {
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                if(response.isSuccessful()){
+                    Log.d("getTopHeadLines", response.body().toString());
+                }else{
+                    Log.d("getTopHeadLines", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Log.d("getTopHeadLines", t.toString());
+            }
+        });
+
     }
 
     @Override
